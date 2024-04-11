9531b863f03729f893da86df1c9e79b15815cd3e
YegorNext <yegor.d2015@gmail.com>
2024-04-11 14:03:56 +0300

    actions && filter

--- java-app-client/src/App.vue
+++ java-app-client/src/App.vue
@@ -1,85 +1,61 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+    import DataTable from '@/components/DataTable.vue'
+
+    export default{
+        components:{
+            DataTable
+        },
+        data(){
+            return{
+                userData: [
+                    { id: "1", Username: "Ngetst", Name: "Yehrs", Phone: "998", Email: "yegor.d02152gmail.com", Date: "23.05.2024", Action: "strst" },
+                    { id: "2", Username: "Uniqeut", Name: "John", Phone: "123", Email: "john@example.com", Date: "24.05.2024", Action: "delete" },
+                    { id: "3", Username: "Taketsy", Name: "Jane", Phone: "456", Email: "jane@example.com", Date: "25.05.2024", Action: "edit" }
+                ]
+            }
+        },
+        methods:{
+            dataDescSort(){
+                this.userData.sort((a, b) => b.id - a.id);
+            },
+            dataAscSort(){
+                this.userData.sort((a, b) => a.id - b.id);
+            }
+        }
+    }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+   <div class = "container-xxl mt-5"> 
+            <h1 class="bold">User Table</h1>
+            <div class = "row">
+                <div class = "col-4 d-flex control-btns">
+                        <div class = "dropdown">
+                            <button class = "btn btn-secondary dropdown-toggle" type = "button" id = "actionDropdown" data-bs-toggle="dropdown" aria-expanded="false">Actions</button>
+                            <ul class = "dropdown-menu" aria-labelledby="actionDropdown">
+                                <li><a class="dropdown-item" href="#">Delete</a></li>
+                                <li><a class="dropdown-item" href="#">Import Excel</a></li>
+                                <li><a class="dropdown-item" href="#">Export Excel</a></li>
+                            </ul>
+                        </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+                        <div class = "dropdown">
+                            <button class = "btn btn-secondary dropdown-toggle" type = "button" id = "filterDropdown" data-bs-toggle="dropdown" aria-expanded="false">Filter</button>
+                            <ul class = "dropdown-menu" aria-labelledby="filterDropdown">
+                                <li><a class="dropdown-item" href="#" @click="dataAscSort()">By ID (Ascending Order)</a></li>
+                                <li><a class="dropdown-item" href="#" @click="dataDescSort()">By ID (Descending Order)</a></li>
+                            </ul>
+                        </div>
+                </div>
+            </div>
+            <div class = "container-xxl">
+                <div class = "overflow-container">
+                    <DataTable :data = "userData"> </DataTable>
+                </div>
+             </div>
+      </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ java-app-client/src/components/DataTable.vue
@@ -0,0 +1,32 @@
+<script>
+    export default{
+        props: ['data']
+    }
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Date</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in data" :key="item.id">
+                <td>{{ item.id }}</td>
+                <td>{{ item.Username }}</td>
+                <td>{{ item.Name }}</td>
+                <td>{{ item.Phone }}</td>
+                <td>{{ item.Email }}</td>
+                <td>{{ item.Date }}</td>
+                <td>{{ item.Action }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
