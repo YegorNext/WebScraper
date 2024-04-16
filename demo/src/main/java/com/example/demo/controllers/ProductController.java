0ca59f7448bf4bfd37c0ceadb9beffa8457faa0b
YegorNext <yegor.d2015@gmail.com>
2024-04-16 19:43:06 +0300

    structure updated

---
+++ demo/src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProductController {
+
+}
