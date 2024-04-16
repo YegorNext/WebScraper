a709a9612e63faa2d7877b8b8e69636dd761fbb8
YegorNext <yegor.d2015@gmail.com>
2024-04-16 20:54:58 +0300

    controllers added

--- demo/src/main/java/com/example/demo/controllers/ProductController.java
+++ demo/src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,10 +1,49 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.service.annotation.PatchExchange;
 
+import com.example.demo.entity.Product;
+import com.example.demo.repository.IProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private IProductRepository pRepository;
+
+    @GetMapping(path = "/", produces = "application/json")
+    @ResponseBody
+    public Iterable<Product> getProducts() {
+        return pRepository.findAll();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> createProductsList(@RequestBody String url) {
+
+        return ResponseEntity.ok("List successfully added");
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long productId) {
+
+        if (pRepository.existsById(productId)) {
+            pRepository.deleteById(productId);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().build();
+    }
 }
